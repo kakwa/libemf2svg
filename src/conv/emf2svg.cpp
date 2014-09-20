89a4514d2c0e76086c664dcde221834778c38366
Carpentier F.-G <carpenti@abigorneau-5.local>
2014-09-20 08:47:40 +0200

    dirty fix on emf2svg args counter

--- src/conv/emf2svg.cpp
+++ src/conv/emf2svg.cpp
@@ -89,7 +89,7 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
       break;
 
     case ARGP_KEY_END:
-      if (state->arg_num == 0)
+      if (state->arg_num < 0)
         /* Not enough arguments. */
         argp_usage (state);
       break;
