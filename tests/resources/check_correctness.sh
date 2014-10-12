bb6c613e256e5a19acffe1d84c4780ca32795fa6
kakwa <carpentier.pf@gmail.com>
2014-10-13 00:48:04 +0200

    adding valgrind test correctly

--- tests/resources/check_correctness.sh
+++ tests/resources/check_correctness.sh
@@ -27,10 +27,15 @@ mkdir -p $OUTDIR
 for emf in `find $EMFDIR -name "*.emf"`
 do
     verbose_print "\n############## `basename ${emf}` ####################"
-    ../../emf2svg -p -w 800 -h 600 -i $emf -o ${OUTDIR}/`basename ${emf}`.svg $VERBOSE_OPT
     verbose_print "Command: ../../emf2svg -p -i $emf -o ${OUTDIR}/`basename ${emf}`.svg"
+    valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --error-exitcode=1 \
+    ../../emf2svg -p -w 800 -h 600 -i $emf -o ${OUTDIR}/`basename ${emf}`.svg $VERBOSE_OPT
+    if [ $? -ne 0 ]
+    then
+        printf "[ERROR] emf2svg memleaked or crash converting emf '$emf'\n"
+        ret=1
+    fi
     xmllint --dtdvalid ./svg11-flat.dtd  --noout ${OUTDIR}/`basename ${emf}`.svg
-    valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --error-exitcode=1 ../../emf2svg -p -w 800 -h 600 -i $emf -o ${OUTDIR}/`basename ${emf}`.svg $VERBOSE_OPT
     if [ $? -ne 0 ]
     then
         printf "[ERROR] emf2svg generate bad svg '${OUTDIR}/`basename ${emf}`.svg' from emf '$emf'\n"
