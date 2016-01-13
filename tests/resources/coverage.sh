ef84b28eaefd03ab33d14b8ff6785f49d8ca5076
kakwa <carpentier.pf@gmail.com>
2016-01-13 09:24:20 +0100

    modified coverage.sh to output markdown

--- tests/resources/coverage.sh
+++ tests/resources/coverage.sh
@@ -2,7 +2,10 @@
 
 
 cd `dirname $0`
+if ! [ "$1" = "nocolor" ]
+then
 . ./colors.sh
+fi
 
 percent(){
     printf "$(( $1 * 100 / $2  ))"
@@ -15,12 +18,18 @@ count_emf_IGNORED=`grep -c FLAG_IGNORED $EMFC`
 count_emf_UNUSED=`grep -c FLAG_UNUSED $EMFC`
 count_emf_PARTIAL=`grep -c FLAG_PARTIAL $EMFC`
 total_emf=$(( $count_emf_SUPPORTED + $count_emf_IGNORED + $count_emf_UNUSED + $count_emf_PARTIAL  ))
-printf "EMF RECORDS:\n"
-printf "* ${BGre}Supported: ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_SUPPORTED" "`percent $count_emf_SUPPORTED $total_emf`"
-printf "* ${BYel}Partial:   ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_PARTIAL"   "`percent $count_emf_PARTIAL   $total_emf`"
-printf "* ${BBlu}Unused:    ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_UNUSED"    "`percent $count_emf_UNUSED    $total_emf`"
-printf "* ${BRed}Ignored:   ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_IGNORED"   "`percent $count_emf_IGNORED   $total_emf`"
-printf "* Total:    % 3d\n" $total_emf
+
+printf "EMF/EMF+ record type coverage\n"
+printf "%s\n\n" "-----------------------------"
+printf "EMF RECORDS:\n\n"
+
+printf "|   Status  | Count | Percent |\n"
+printf "|:---------:|:-----:|:-------:|\n"
+printf "| ${BGre}Supported ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_SUPPORTED" "`percent $count_emf_SUPPORTED $total_emf`"
+printf "| ${BYel}Partial   ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_PARTIAL"   "`percent $count_emf_PARTIAL   $total_emf`"
+printf "| ${BBlu}Unused    ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_UNUSED"    "`percent $count_emf_UNUSED    $total_emf`"
+printf "| ${BRed}Ignored   ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_IGNORED"   "`percent $count_emf_IGNORED   $total_emf`"
+printf "| Total     |  %3d  |         |\n" $total_emf
 
 
 printf "\n"
@@ -30,11 +39,13 @@ count_emf_IGNORED=`grep -c FLAG_IGNORED     $PMFC`
 count_emf_UNUSED=`grep -c FLAG_UNUSED       $PMFC`
 count_emf_PARTIAL=`grep -c FLAG_PARTIAL     $PMFC`
 total_emf=$(( $count_emf_SUPPORTED + $count_emf_IGNORED + $count_emf_UNUSED + $count_emf_PARTIAL  ))
-printf "EMF+ RECORDS:\n"
-printf "* ${BGre}Supported: ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_SUPPORTED" "`percent $count_emf_SUPPORTED $total_emf`"
-printf "* ${BYel}Partial:   ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_PARTIAL"   "`percent $count_emf_PARTIAL   $total_emf`"
-printf "* ${BBlu}Unused:    ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_UNUSED"    "`percent $count_emf_UNUSED    $total_emf`"
-printf "* ${BRed}Ignored:   ${RCol}% 3d [${BCya}% 3d%%${RCol}]\n"  "$count_emf_IGNORED"   "`percent $count_emf_IGNORED   $total_emf`"
-printf "* Total:     % 3d\n" $total_emf
+printf "EMF+ RECORDS:\n\n"
 
+printf "|   Status  | Count | Percent |\n"
+printf "|:---------:|:-----:|:-------:|\n"
+printf "| ${BGre}Supported ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_SUPPORTED" "`percent $count_emf_SUPPORTED $total_emf`"
+printf "| ${BYel}Partial   ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_PARTIAL"   "`percent $count_emf_PARTIAL   $total_emf`"
+printf "| ${BBlu}Unused    ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_UNUSED"    "`percent $count_emf_UNUSED    $total_emf`"
+printf "| ${BRed}Ignored   ${RCol}|  % 3d  | [${BCya}% 3d%%${RCol}]  |\n"  "$count_emf_IGNORED"   "`percent $count_emf_IGNORED   $total_emf`"
+printf "| Total     |  %3d  |         |\n" $total_emf
 
