9a1c9eb0050478a88b26b63fb716a8c2d90397e4
Cian Chambliss <cianchambliss@Cians-MBP.netgear.com>
2016-05-01 10:41:05 -0400

    lost headers

---
+++ inc/emf2svg.h
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h> /* for offsetof() macro */
+#include <string.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// structure containing generator arguments
+typedef struct {
+    // SVG namespace (the '<something>:' before each fields)
+    char *nameSpace;
+    // Verbose mode, output fields and fields values if True
+    bool verbose;
+    // Handle emf+ records or not
+    bool emfplus;
+    // draw svg document delimiter or not
+    bool svgDelimiter;
+    // height of the target image
+    double imgHeight;
+    // width of the target image
+    double imgWidth;
+} generatorOptions;
+
+// covert function
+int emf2svg(char *contents, size_t length, char **out,
+            generatorOptions *options);
+//! \endcond
+
+#ifdef __cplusplus
+}
+#endif
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
